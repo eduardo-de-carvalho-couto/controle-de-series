4980fb669556b489e355386a97a855af16878f8a
Eduardo <coutoeduz@gmail.com>
2022-06-10 02:26:26 -0300

    Finalizando a parte de relacionamentos

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Episode extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
+    public function season()
+    {
+        return $this->belongsTo(Season::class);
+    }
 }
--- app/Models/Season.php
+++ app/Models/Season.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Season extends Model
 {
     use HasFactory;
+
+    public function series()
+    {
+        return $this->belongsTo(Serie::class);
+    }
+
+    public function episodes()
+    {
+        return $this->hasMany(Episode::class);
+    }
 }
