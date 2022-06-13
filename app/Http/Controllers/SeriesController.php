4c1046ce87cf49ca0422e22f24a4f4c9d5b8eee0
Eduardo <coutoeduz@gmail.com>
2022-06-13 16:09:07 -0300

    Criando episodios

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
 use Illuminate\Http\Request;
-use App\Models\Serie;
+use App\Models\{Series, Season, Episode};
 
 class SeriesController extends Controller
 {
     public function index(Request $request){
         
-        $series = Serie::all();
+        $series = Series::all();
         $mensagemSucesso = session('mensagem.sucesso');
 
         return view('series.index')->with('series', $series)
@@ -24,13 +24,31 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = Serie::create($request->all());
+        $serie = Series::create($request->all());
+        for($i = 1; $i <= $request->seasonsQty ; $i++){
+            $seasons[] = [
+                'series_id' => $serie->id,
+                'number' => $i,
+            ];
+        }
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach ($serie->seasons as $season){
+            for($j = 1; $j <= $request->episodesPerSeason ; $j++){
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $j
+                ];
+            }
+        }
+        Episode::insert($episodes);
         
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
 
-    public function destroy(Serie $series)
+    public function destroy(Series $series)
     {
         $series->delete();
 
@@ -38,12 +56,12 @@ class SeriesController extends Controller
             ->with('mensagem.sucesso', 'Série removida com sucesso');
     }
 
-    public function edit(Serie $series)
+    public function edit(Series $series)
     {
         return view('series.edit')->with('serie', $series);
     }
 
-    public function update(Serie $series, SeriesFormRequest $request)
+    public function update(Series $series, SeriesFormRequest $request)
     {
         $series->fill($request->all());
         $series->save();
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\{SeriesController, SeasonsController};
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +20,6 @@ Route::get('/', function () {
 
 Route::resource('/series', SeriesController::class)
     ->except(['show']);
+
+Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
 
