dde438e6828ffe0bb68a76161baa67e433455e1b
Eduardo <coutoeduz@gmail.com>
2022-06-15 10:22:58 -0300

    Injecao de dependencia

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use App\Models\{Series, Season, Episode};
+use App\Models\{Series};
+use App\Repositories\SeriesRepository;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $repository)
+    {
+    }
+
     public function index(Request $request){
         
         $series = Series::all();
@@ -25,30 +29,8 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = DB::transaction(function () use ($request, &$serie) {
-            $serie = Series::create($request->all());
-            $seasons = [];
-            for($i = 1; $i <= $request->seasonsQty ; $i++){
-                $seasons[] = [
-                    'series_id' => $serie->id,
-                    'number' => $i,
-                ];
-            }
-            Season::insert($seasons);
 
-            $episodes = [];
-            foreach ($serie->seasons as $season){
-                for($j = 1; $j <= $request->episodesPerSeason ; $j++){
-                    $episodes[] = [
-                        'season_id' => $season->id,
-                        'number' => $j
-                    ];
-                }
-            }
-            Episode::insert($episodes);
-
-            return $serie;
-        });
+        $serie = $this->repository->add($request);
         
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
